ed36fb46e3de639e4895bd2e99c8b30b0f7e4ba0
OrangeDrangon <25403517+OrangeDrangon@users.noreply.github.com>
2025-07-23 22:35:07 -0400

    add devenv

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [ 
+      pkgs.gitFull
+      pkgs.gnumake
+      
+      pkgs.nixd
+      
+      
+  ];
+  
+  languages.nix.enable = true;
+
+  languages.python.enable = true;
+  languages.python.uv.enable = true;
+  languages.python.uv.sync.enable = true;
+  languages.python.venv.enable = true;
+}
