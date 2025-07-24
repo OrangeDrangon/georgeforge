8db82c43234743ec8487f5203b2b32da91b931b6
OrangeDrangon <25403517+OrangeDrangon@users.noreply.github.com>
2025-07-23 22:53:17 -0400

    Use devenv for precommit

--- devenv.nix
+++ devenv.nix
@@ -5,9 +5,7 @@
       pkgs.gitFull
       pkgs.gnumake
       
-      pkgs.nixd
-      
-      
+      pkgs.nixd 
   ];
   
   languages.nix.enable = true;
@@ -17,4 +15,21 @@
   languages.python.uv.sync.enable = true;
   languages.python.uv.sync.allGroups = true;
   languages.python.venv.enable = true;
+  
+  git-hooks.hooks = {
+      black.enable = true;
+      flake8.enable = true;
+      check-shebang-scripts-are-executable.enable = true;
+      check-symlinks.enable = true;
+      check-yaml.enable = true;
+      check-merge-conflicts.enable = true;
+      check-json.enable = true;
+      check-executables-have-shebangs.enable = true;
+      check-added-large-files.enable = true;
+      check-case-conflicts.enable = true;
+      markdownlint.enable = true;
+      nixpkgs-fmt.enable = true;
+      prettier.enable = true;
+      trufflehog.enable = true;
+  };
 }
