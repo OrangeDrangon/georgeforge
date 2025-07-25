8c2da63ed35179a97c9aced007bcc48d3ebc7f3f
OrangeDrangon <25403517+OrangeDrangon@users.noreply.github.com>
2025-07-24 20:35:46 -0400

    use app settings to configure what we preload for for sale creation (#11)

--- georgeforge/management/commands/georgeforge_eveuniverse_load.py
+++ georgeforge/management/commands/georgeforge_eveuniverse_load.py
@@ -1,28 +1,13 @@
+# Standard Library
+import itertools
+
 # Django
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 # George Forge
 from georgeforge import __title__
-
-# Eve Category IDs
-MATERIAL = 4
-SHIP = 6
-MODULE = 7
-CHARGE = 8
-BLUEPRINT = 9
-SKILLS = 16
-COMMODITY = 17
-DRONE = 18
-IMPLANT = 20
-ASTEROID = 25
-APPAREL = 30
-PLANETARY_INDUSTRY = 41
-PLANETARY_COMMODITIES = 43
-SPECIAL_EDITION_ASSETS = 63
-STRUCTURES = 63
-STRUCTURE_MODULE = 66
-SKINS = 91
+from georgeforge.app_settings import FORGE_CATEGORIES
 
 
 class Command(BaseCommand):
@@ -37,23 +22,15 @@ class Command(BaseCommand):
         :param **options:
 
         """
+
+        categories: list[list[str]] = [
+            ["--category_id", str(x)] for x in FORGE_CATEGORIES
+        ]
+
         call_command(
             "eveuniverse_load_types",
             "--noinput",
             __title__,
-            "--category_id",
-            str(SHIP),
-            "--category_id",
-            str(MODULE),
-            "--category_id",
-            str(CHARGE),
-            "--category_id",
-            str(DRONE),
-            "--category_id",
-            str(IMPLANT),
-            "--category_id",
-            str(STRUCTURES),
-            "--category_id",
-            str(STRUCTURE_MODULE),
+            *itertools.chain.from_iterable(categories),
         )
         call_command("eveuniverse_load_data", "--noinput", "map")
